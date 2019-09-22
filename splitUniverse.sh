3ecab098374c65d86dc854d1fa58b1a7b1dd94da
caseypancoast <cqpancoast@gmail.com>
2019-09-22 19:26:02 -0400

    Added function to call this stuff

---
+++ splitUniverse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## UNIVERSE SPLITTER ##
+# Queries ANU's QRNG thing and spits out either a zero or a one for no arguments, or if you do supply an argument it takes the huge-ass number mod whatever that is.
+
+modBy=2
+if ! [ -z $1 ]
+	then
+	modBy=$1
+fi
+if (($modBy < "1"))
+        then
+        echo "Mod argument must be 2 or above, got "
+	echo $modBy
+fi
+bigAssNumber=$(python ~/Code/GitHub/qrng/quantumRNG.py)
+if (($bigAssNumber < 0))
+	then
+	bigAssNumber=$bigAssNumber*-1
+fi
+echo $(($bigAssNumber % $modBy))
+
